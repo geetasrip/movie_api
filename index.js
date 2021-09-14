e15540ec0116fe8e76b35ce631eb422154852605
geetasri p <geeta.peddireddi@gmail.com>
2021-09-14 13:46:59 -0700

    updated connection URL env variable

--- index.js
+++ index.js
@@ -12,10 +12,13 @@ const express = require("express"),
 const app = express();
 const { check, validationResult } = require("express-validator");
 
-mongoose.connect("mongodb://localhost:27017/myFlixDB", {
+/*mongoose.connect("mongodb://localhost:27017/myFlixDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+});*/
+
+mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 /*mongoose.connect(process.env.CONNECTION_URI, {
   useNewUrlParser: true,
